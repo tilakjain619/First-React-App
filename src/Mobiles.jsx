2178a61dcf8e9868621ced8588126044f64812b5
tilakjain619 <frontendchamp123@gmail.com>
2024-06-30 15:14:57 +0530

    add cart

--- src/Mobiles.jsx
+++ src/Mobiles.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MobilesList from './MobilesList'
 import MobileData from './mobiles.json';
 
 export default function Mobiles() {
+  const [cart, setCart] = useState([]);
+  const [totalCount, setTotalCount] = useState(0);
+
+  function handleAddToCart(item) {
+    setCart([...cart, item]);
+    setTotalCount(totalCount + item.count);
+  }
   return (
     <>
     <div className="title">
           Popular Mobiles
+        </div>
+        <div>
+        <p>Total items in cart: {totalCount}</p>
+      <ul>
+        {cart.map((item, index) => (
+          <li key={index}>
+            {item.name} x {item.count}
+          </li>
+        ))}
+      </ul>
         </div>
       <div className='mobileContainer'>
       {MobileData.map((ele) => (
@@ -15,6 +32,7 @@ export default function Mobiles() {
           name={ele.name}
           image={ele.image}
           price={ele.price}
+          onAddToCart={handleAddToCart}
         />
       ))}
     </div>
--- src/MobilesList.jsx
+++ src/MobilesList.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MobilesList.css';
 
 export default function MobilesList(props) {
-    let {name, image, price} = props;
+  const { name, image, price, onAddToCart } = props;
+  const [count, setCount] = useState(0);
+
+  function handleDecrement() {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      setCount(0);
+    }
+  }
+
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+
+  function handleAddToCart() {
+    onAddToCart({ name, image, price, count });
+  }
+
   return (
     <div className='card'>
       <img src={image} alt={name} />
-      <div>
+      <div className='mobile'>
         <h2>{name}</h2>
         <p>Rs {price}</p>
-        <button>Add to cart</button>
+        <div className='cart-options'>
+          <button onClick={handleDecrement}>-</button>
+          <span>{count}</span>
+          <button onClick={handleIncrement}>+</button>
+        </div>
+        <button onClick={handleAddToCart} className='add-to-cart'>
+          Add to cart
+        </button>
       </div>
     </div>
-  )
+  );
 }
